2d3e0475d1ee1a1de9308db1c70458a7cb413f0c
Aditya Manthramurthy <aditya.mmy@gmail.com>
2022-11-10 12:14:04 -0800

    Fix backup scripts

--- backup-restore/03-restore-sample-db-from-backup.sql
+++ backup-restore/03-restore-sample-db-from-backup.sql
@@ -1,9 +1,22 @@
 -- Restore script
 
--- Delete the local db first, to restore it again from backup
-DROP DATABASE [SQLTestDB];
+USE master;
+GO
 
+-- Delete the local db first, to restore it again from backup
+DROP DATABASE IF EXISTS [SQLTestDB];
+GO
+
+-- Restore
 RESTORE DATABASE [SQLTestDB]
 FROM    URL = 's3://15.15.15.185:9000/sqltest/sqltestdb_01.bak'
 WITH    REPLACE -- overwrite
 ,       STATS               = 10;
+GO
+
+-- Switch to restored db and show some contents.
+USE [SQLTestDB];
+GO
+
+SELECT * FROM SQLTest;
+GO
